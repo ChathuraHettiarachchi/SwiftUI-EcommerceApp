8f32e45b8595ad47e067ba779b802ee27e9540b6
Chathura Hettiarachchi <chathura93@yahoo.com>
2023-08-31 09:09:51 +1200

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Chathura Hetti Arachchi on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
